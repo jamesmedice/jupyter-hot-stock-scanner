3a1415e2b110e9d75052f1b3f00fe14b3859a7c0
GFT\a73s <tiago.medice@gft.com>
2025-11-26 22:02:18 +0100

    fix output

---
+++ scripts/upload_reports_2_firebase.py
@@ -0,0 +1,15 @@
+# scripts/upload_to_firebase.py
+import os
+import json
+import glob
+from pathlib import Path
+import firebase_admin
+from firebase_admin import credentials, storage
+import upload_to_firebase
+from upload_to_firebase import init_bucket, upload_folder_local
+
+bucket = init_bucket()
+# upload daily, weekly, monthly if present
+upload_folder_local(bucket, "output/hourly", remote_prefix="market/")
+upload_folder_local(bucket, "output/topscore", remote_prefix="market/")
+upload_folder_local(bucket, "output/topheatmap", remote_prefix="market/")

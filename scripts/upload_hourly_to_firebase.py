26bc2908c7f2ca08f9ce445abecdd9aec011cc10
GFT\a73s <tiago.medice@gft.com>
2025-11-26 10:55:45 +0100

    fix output

--- scripts/upload_hourly_to_firebase.py
+++ scripts/upload_hourly_to_firebase.py
@@ -9,3 +9,7 @@ from upload_to_firebase import init_bucket, upload_folder_local
 
 bucket = init_bucket()
 upload_folder_local(bucket, "output/hourly", remote_prefix="market/")
+
+upload_folder_local(bucket, "output/top20", remote_prefix="market/")
+upload_folder_local(bucket, "output/recommendations", remote_prefix="market/")
+upload_folder_local(bucket, "output/appearances", remote_prefix="market/")
--- scripts/upload_runtime_to_firebase.py
+++ scripts/upload_runtime_to_firebase.py
@@ -9,3 +9,9 @@ from upload_to_firebase import init_bucket, upload_folder_local
 
 bucket = init_bucket()
 upload_folder_local(bucket, "output/runtime", remote_prefix="market/")
+
+upload_folder_local(bucket, "output/hotstocks", remote_prefix="market/")
+upload_folder_local(bucket, "output/hotscore", remote_prefix="market/")
+upload_folder_local(bucket, "output/volumespike", remote_prefix="market/")
+upload_folder_local(bucket, "output/momentum", remote_prefix="market/")
+

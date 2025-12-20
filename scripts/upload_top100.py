765f1076d231276df08ede530586a39183865f55
GFT\a73s <tiago.medice@gft.com>
2025-12-20 22:24:30 +0100

    new jupyter

--- scripts/upload_top100.py
+++ scripts/upload_top100.py
@@ -11,4 +11,3 @@ from upload_to_firebase import init_bucket, upload_folder_local, upload_folder_l
 bucket = init_bucket()
 # upload daily, weekly, monthly if present
 upload_folder_local_withCSV(bucket, "output/top100", remote_prefix="market/")
-upload_folder_local(bucket, "output/top100/charts", remote_prefix="market/")

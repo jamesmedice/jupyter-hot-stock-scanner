a1b5fbb7cf2bc95b8014dfdb42c5c91a35af1c63
GFT\a73s <tiago.medice@gft.com>
2026-01-04 12:36:29 +0100

    ....

---
+++ scripts/upload_classification.py
@@ -0,0 +1,13 @@
+# scripts/upload_to_firebase.py
+import os
+import json
+import glob
+from pathlib import Path
+import firebase_admin
+from firebase_admin import credentials, storage
+import upload_to_firebase
+from upload_to_firebase import init_bucket, upload_folder_local, upload_folder_local_withCSV
+
+bucket = init_bucket()
+# upload daily, weekly, monthly if present
+upload_folder_local_withCSV(bucket, "output/classification", remote_prefix="market/")

f62fbf8235a85a32ca14fb61a8aab5066919bfb5
GFT\a73s <tiago.medice@gft.com>
2026-01-06 15:52:41 +0100

    ....

--- scripts/upload_trend.py
+++ scripts/upload_trend.py
@@ -9,4 +9,4 @@ import upload_to_firebase
 from upload_to_firebase import init_bucket, upload_folder_local, upload_folder_local_withCSV
 
 bucket = init_bucket()
-upload_folder_local_withCSV(bucket, "output/dividends", remote_prefix="market/")
+upload_folder_local_withCSV(bucket, "output/trend", remote_prefix="market/")
